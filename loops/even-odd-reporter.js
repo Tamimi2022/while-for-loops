f0393c377c87bbf416c0fded9ca813f3d13b7e4a
Farid Attamimi <97972332+Tamimi2022@users.noreply.github.com>
2022-02-08 19:23:26 +0100

    Add files via upload

---
+++ loops/even-odd-reporter.js
@@ -0,0 +1,9 @@
+// Even Odd Reporter
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " this is even");
+    }
+    else {
+        console.log(i + " this is odd");
+    }
+}
---
+++ loops/multiplication-tables.js
@@ -0,0 +1,14 @@
+// Multiplication tables
+const multi = 9;
+for (let i = 1; i <=10; i++) {
+    let result = multi * i;
+    console.log(multi + " * " + i + " = " + result);
+}
+
+// Bonus 
+for (let multi = 1; multi <= 10; multi++) {
+for (let i = 1; i <= 10; i++) {
+    let result = multi * i;
+    console.log(multi + " * " + i + " = " + result);
+}
+}
